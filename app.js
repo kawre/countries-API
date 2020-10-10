1764f73b678bfbfc0d8784e26228a85cb86bb87e
kawre <69250723+kawre@users.noreply.github.com>
2020-10-10 14:27:30 +0200

    js

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-const showFilters = document.querySelector('.show-filters');
-const filters = document.querySelector('.filters');
+const showFilters = document.querySelector(".filter-current");
+const filters = document.querySelector(".filters");
 
-showFilters.addEventListener('click', function(){
-filters.classList.toggle('active');
+showFilters.addEventListener("click", function () {
+  filters.classList.toggle("active");
 });
 
 // API
 
-fetch('https://restcountries.eu/rest/v2/all')
-.then(res => res.json())
-.then(data => console.log(data))
+function getCountries() {
+  fetch("https://restcountries.eu/rest/v2/all")
+    .then((res) => res.json())
+    .then((data) => {
+      let countriesData = ``;
+      data.forEach(function (country) {
+        countriesData += `<article class="country">
+        <div class="country-img">
+            <img src="${country.flag}" alt="${country.name}">
+        </div>
+        <div class="country-about">
+            <h2>${country.name}</h2>
+            <div class="country-details">
+                <p>Population: <span>${country.population}</span></p>
+                <p>Region: <span>${country.region}</span></p>
+                <p>Capital: <span>${country.capital}</span></p>
+            </div>
+        </div>
+    </article>`;
+      });
+      document.querySelector(".countries").innerHTML = countriesData;
+    });
+}
+
+const countriesContainer = document.querySelector("counties");
+
+window.addEventListener("DOMContentLoaded", () => {
+  getCountries();
+});
