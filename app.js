82f2887cc5ac0c519001c3c893c903d5e549ecce
kawre <69250723+kawre@users.noreply.github.com>
2020-10-10 16:37:20 +0200

    improvements
    added loading animation and images are now responsive,

--- app.js
+++ app.js
@@ -7,32 +7,40 @@ showFilters.addEventListener("click", function () {
 
 // API
 
+const countiresContainer = document.querySelector(".countries");
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", () => {
+  countiresContainer.classList.remove("hide");
+  preloader.classList.add("hide");
+});
+
+// getCountries
+
 function getCountries() {
+  let loader = `<img class="preloader" src="5.gif" alt="Loading" />`;
+  document.querySelector(".countries").innerHTML = loader;
   fetch("https://restcountries.eu/rest/v2/all")
     .then((res) => res.json())
     .then((data) => {
       let countriesData = ``;
       data.forEach(function (country) {
         countriesData += `<article class="country">
-        <div class="country-img">
+            <div class="country-img">
             <img src="${country.flag}" alt="${country.name}">
-        </div>
-        <div class="country-about">
+            </div>
+            <div class="country-about">
             <h2>${country.name}</h2>
             <div class="country-details">
-                <p>Population: <span>${country.population}</span></p>
-                <p>Region: <span>${country.region}</span></p>
-                <p>Capital: <span>${country.capital}</span></p>
+            <p>Population: <span>${country.population}</span></p>
+            <p>Region: <span>${country.region}</span></p>
+            <p>Capital: <span>${country.capital}</span></p>
             </div>
-        </div>
-    </article>`;
+            </div>
+            </article>`;
+        document.querySelector(".countries").innerHTML = countriesData;
       });
-      document.querySelector(".countries").innerHTML = countriesData;
     });
 }
 
-const countriesContainer = document.querySelector("counties");
-
-window.addEventListener("DOMContentLoaded", () => {
-  getCountries();
-});
+getCountries();
